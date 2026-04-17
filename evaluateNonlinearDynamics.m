90ea5b5d431fec6a88ddf239ba5b957ec4ac931d
Cole Shaigec <coleshaigec@crc-dot1x-nat-10-239-73-95.bu.edu>
2026-04-17 10:17:07 -0400

    Implement utility to build nonlinear plant model

---
+++ evaluateNonlinearDynamics.m
@@ -0,0 +1,27 @@
+function xDot = evaluateNonlinearDynamics(x, u, d, plantGeometry, physicalConstants)
+    % EVALUATENONLINEARDYNAMICS Evaluates nonlinear plant dynamics for use by ODE45.
+    %
+    % AUTHOR: Richie Kim/Dani Schwartz
+    %
+    % INPUTS
+    %
+    %
+    % OUTPUTS
+
+
+    %%%%%%%%%%%%%%%%%%%%%%%%
+    % IMPLEMENTATION NOTES %
+    %%%%%%%%%%%%%%%%%%%%%%%%
+    % Please don't delete the docstring above. 
+    %
+    % This function builds a nonlinear plant object that packages the
+    % nonlinear dynamics as a computable function usable by the simulator. 
+    % 
+    % The nonlinear plant object will also carry relevant metadata. 
+    % This skeleton packages the anonymous function for evaluating
+    % nonlinear dynamics already, so your task here is to add the metadata
+    % and to implement evaluateNonlinearDynamics in a separate file.
+    
+end
+
+
