8a7f87c0513bb7c42ec0bc530484b6362db70c79
Cole Shaigec <coleshaigec@crc-dot1x-nat-10-239-73-95.bu.edu>
2026-04-20 15:55:49 -0400

    Implement utility to set initial conditions

---
+++ src/config/setInitialConditions.m
@@ -0,0 +1,18 @@
+function initialConditions = setInitialConditions()
+    % SETINITIALCONDITIONS Allows user to hard-code initial conditions for closed-loop simulation.
+    %
+    % AUTHOR: Cole H. Shaigec
+    %
+    % OUTPUT
+    %  initialConditions struct with fields
+    %      .x0 (2 x 1 double) - initial state
+    %      .xHat0 (2 x 1 double) - initial state estimate
+    %
+    % NOTES
+    % - xHat0 is used for the 'moldOnly' observability case.
+
+    initialConditions = struct();
+    initialConditions.x0 = [1;1];
+    initialConditions.xHat0 = [1,1];
+
+end
