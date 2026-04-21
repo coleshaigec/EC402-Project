8052563b16051d8db1df5502c13ca8f6f8c9d122
Cole Shaigec <coleshaigec@crc-dot1x-nat-10-239-69-105.bu.edu>
2026-04-21 09:22:53 -0400

    Implement utility to compute settling time

---
+++ src/utilities/constants/getSettlingThresholdForMoldLevel.m
@@ -0,0 +1,10 @@
+function settlingThreshold = getSettlingThresholdForMoldLevel()
+    % GETSETTLINGTHRESHOLDFORMOLDLEVEL Returns settling threshold for mold level.
+    %
+    % AUTHOR: Cole H. Shaigec
+    %
+    % OUTPUT
+    %  settlingThreshold (double)  - mold level settling threshold, in m
+
+    settlingThreshold = 0.002;
+end
