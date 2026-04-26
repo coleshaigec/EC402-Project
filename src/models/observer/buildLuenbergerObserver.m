24ac5860845233e71dd5b97b01855ff4d873f417
Cole Shaigec <coleshaigec@Coles-MacBook-Air-3.local>
2026-04-26 19:04:02 -0400

    Implement skeleton for Luenberger observer

---
+++ src/models/observer/buildLuenbergerObserver.m
@@ -0,0 +1,18 @@
+function luenbergerObserver = buildLuenbergerObserver()
+    % BUILDLUENBERGEROBSERVER Constructs Luenberger observer from system dynamics and measurement model.
+    %
+    % AUTHOR: Cole H. Shaigec
+    %
+    % INPUTS
+    % 
+    %
+    % OUTPUTS
+    %  luenbergerObserver struct with fields
+    %      .L
+    %   
+
+
+    % NEED TO TAKE IN DESIRED OBSERVER POLES
+
+
+end
