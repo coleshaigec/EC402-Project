d7a081cc77373bcfe8e1fe92e5d2f77dd34808c8
Cole Shaigec <coleshaigec@crc-dot1x-nat-10-239-73-95.bu.edu>
2026-04-27 11:39:46 -0400

    Implement utility to get desired observer poles

---
+++ src/config/getDesiredObserverPoles.m
@@ -0,0 +1,10 @@
+function desiredObserverPoles = getDesiredObserverPoles()
+    % GETDESIREDOBSERVERPOLES Returns desired poles for Luenberger observer.
+    %
+    % AUTHOR: Cole H. Shaigec
+    %
+    % OUTPUT
+    %  desiredObserverPoles (2 x 1 double) - desired observer error dynamics poles
+
+    desiredObserverPoles = [-5; -6];
+end
